b7f69e434500b93bb5121eae555593a34164985b
Kumar <krishnakik.3.141@gmail.com>
2025-11-23 18:58:45 +0400

    complete week 7: secure authentication system with bcrypt

--- auth.py
+++ auth.py
@@ -1,1 +1,12 @@
-print("Hello World")
+import bcrypt
+import os
+def hash_password(plain_text_password):
+    password_bytes = plain_text_password.encode("utf-8")
+    salt = bcrypt.gensalt()
+    hashed = bcrypt.hashpw(password_bytes, salt)
+    return hashed.decode("utf-8")
+def verify_password(plain_text_password, hashed_password):
+    password_bytes = plain_text_password.encode("utf-8")
+    hashed_bytes = hashed_password.encode("utf-8")
+    return bcrypt.checkpw(password_bytes, hashed_bytes)
+
